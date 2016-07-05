c53a82f90336fddbcb5c6bab630fe72f462fff57
Fábio <fabio.paiva@praxis.com.br>
2016-07-05 17:50:07 -0300

    Update URL

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author='mike wakerly',
     author_email='opensource@hoho.com',
     packages = find_packages(),
-    url='https://github.com/mik3y/django-db-multitenant',
+    url='https://github.com/fabiopaiva-praxis/django-db-multitenant',
     license='BSD',
     description='Multitenant support for Django, using one tenant per database.',
     long_description=open(README).read() if exists(README) else "",
